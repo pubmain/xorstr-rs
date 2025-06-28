0cccc28c6b3058681f4518dee0ff0de3c90e8275
pubmain <publicmain430@gmail.com>
2025-06-28 11:57:09 +0200

    better sh

--- src/lib.rs
+++ src/lib.rs
@@ -1,13 +1,16 @@
 extern crate proc_macro;
+
+use proc_macro::TokenStream;
 use quote::quote;
-use syn::{LitStr, parse::Parse, parse_macro_input};
+use rand::{Rng, thread_rng};
+use syn::{LitStr, parse::Parse, parse::ParseStream, parse_macro_input};
 
 struct Args {
     input: LitStr,
 }
 
 impl Parse for Args {
-    fn parse(input: syn::parse::ParseStream) -> syn::Result<Self> {
+    fn parse(input: ParseStream) -> syn::Result<Self> {
         Ok(Args {
             input: input.parse()?,
         })
@@ -15,29 +18,42 @@ impl Parse for Args {
 }
 
 #[proc_macro]
-pub fn xorstr(input: proc_macro::TokenStream) -> proc_macro::TokenStream {
-    let args = parse_macro_input!(input as Args);
-    let key_len = rand::random_range(5..10);
-    let mut key_bytes = vec![];
-    for _ in 0..key_len {
-        key_bytes.push(rand::random::<u8>());
-    }
-    let obfuscated_bytes = args
-        .input
-        .value()
+pub fn xorstr(input: TokenStream) -> TokenStream {
+    let Args { input } = parse_macro_input!(input as Args);
+    let value = input.value();
+
+    let mut rng = rand::rng();
+    let key_len = rng.random_range(5..10);
+    let key: Vec<u8> = (0..key_len).map(|_| rng.random::<u8>()).collect();
+
+    let encrypted: Vec<u8> = value
         .bytes()
         .enumerate()
-        .map(|(i, b)| b ^ key_bytes[i % key_bytes.len()])
-        .collect::<Vec<_>>();
+        .map(|(i, b)| b ^ key[i % key_len])
+        .collect();
+
+    let encrypted_len = encrypted.len();
+
+    let encrypted_lits = encrypted.iter().map(|b| quote! { #b });
+    let key_lits = key.iter().map(|b| quote! { #b });
 
     quote! {
         {
-            let data = [#(#obfuscated_bytes),*];
-            let key = [#(#key_bytes),*];
-            let decrypted = data.iter().enumerate()
-                .map(|(i, b)| (b ^ key[i % key.len()]))
-                .collect::<Vec<u8>>();
-            String::from_utf8(decrypted).unwrap().leak()
+            const fn xor(data: &[u8], key: &[u8]) -> [u8; #encrypted_len] {
+                let mut out = [0u8; #encrypted_len];
+                let mut i = 0;
+                while i < data.len() {
+                    out[i] = data[i] ^ key[i % #key_len];
+                    i += 1;
+                }
+                out
+            }
+
+            const DATA: [u8; #encrypted_len] = [#(#encrypted_lits),*];
+            const KEY: [u8; #key_len] = [#(#key_lits),*];
+            const DECRYPTED: [u8; #encrypted_len] = xor(&DATA, &KEY);
+
+            unsafe { ::core::str::from_utf8_unchecked(&DECRYPTED) }
         }
     }
     .into()
